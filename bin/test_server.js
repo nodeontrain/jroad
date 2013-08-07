3740748ea16b8db6d4ef5f33d599ec61ebbdf90f
Dang Thanh <thanhdd.it@gmail.com>
2013-08-07 17:54:33 +0700

    Detect request type

--- bin/test_server.js
+++ bin/test_server.js
@@ -4,17 +4,17 @@ var jroad = require('../lib/jroad');
 http.createServer(function (req, res) {
 	var root_app = process.cwd();
 	var obj = {}
-	var req_parser = jroad.req_parser(req);
-	obj.req_parser = req_parser;
-	if (req_parser.data_type == "asset") {
-		var asset_path = jroad.asset_path(req_parser, root_app);
-		obj.asset_path = asset_path;
-	} else if (req_parser.data_type == "favicon") {
-		var favicon_path = jroad.favicon(root_app);
-		obj.favicon_path = favicon_path;
-	}
+	jroad.req_parser(req, function (req_parser) {
+		obj.req_parser = req_parser;
+		if (req_parser.data_type == "asset") {
+			var asset_path = jroad.asset_path(req_parser, root_app);
+			obj.asset_path = asset_path;
+		} else if (req_parser.data_type == "favicon") {
+			var favicon_path = jroad.favicon(root_app);
+			obj.favicon_path = favicon_path;
+		}
 	
-	res.writeHead(200, {'Content-Type': 'application/json'});
-	res.end(JSON.stringify(obj));
-	
+		res.writeHead(200, {'Content-Type': 'application/json'});
+		res.end(JSON.stringify(obj));
+	});
 }).listen(1337, '127.0.0.1');
